5082324e8142e59966710c357f356418a5ffb192
Rokunuzzaman Bhuiya <rokunuzzamanbhuiya@gmail.com>
2022-09-23 19:00:45 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = require("./app");
+
+mongoose.connect(process.env.DATABASE).then(() => {
+  console.log("data is on");
+});
+
+app.listen(port, () => {
+  console.log("server is running", port);
+});
